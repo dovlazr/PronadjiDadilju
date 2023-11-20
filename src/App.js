a69d7010de39d8d3d0bfd6800d3b1afb2ccb8215
dovlazr <popadicvlad@gmail.com>
2023-11-20 21:09:26 +0100

    added more files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
-function App() {}
+function App() {
+    return(
+        <div>
+            <h1 className="text-teal-400 text-4xl">Hello World!</h1>
+        </div>
+    )
+
+    const macak;
+    
+}
 
 export default App;
